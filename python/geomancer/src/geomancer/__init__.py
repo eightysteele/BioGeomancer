9e232cdb8b82e9c3b124f04b759aaabf13a80487
Aaron Steele <eightysteele@gmail.com>
2011-04-27 12:54:50 -0700

    Updates to LatLng class with some initial unit testing

--- python/geomancer/src/geomancer/__init__.py
+++ python/geomancer/src/geomancer/__init__.py
@@ -1,9 +1,57 @@
+#!/usr/bin/env python
+
+# Copyright 2011 University of California at Berkeley
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+__author__ = "Aaron Steele and John Wieczorek"
+
+"""This module provides classes for calculating georeferencing errors."""
+
 import copy
+import math
+
+from constants import *
 
 class LatLng(object):
-    def __init__(self, lng, lat):
+    
+    @staticmethod
+    def degrees_from_dms(d, m, s):
+        return math.fabs(s / 3600.0) + math.fabs(m / 60.0) + math.fabs(d)
+
+    @staticmethod
+    def degrees_from_dm(d, m):
+        return math.fabs(m / 60.0) + math.fabs(d)
+    
+    @staticmethod
+    def lat_from_dms(d, m, s, direction):
+        lat = LatLng.degress_from_dms(d, m, s)
+        if direction is Direction.SOUTH:
+            lat = -lat
+        return lat
+
+    @staticmethod
+    def lng_from_dms(lngdeg, lngmin, lngsec, lngdir):
+        lng = LatLng.degress_from_dms(d, m, s)
+        if direction is Directions.WEST:
+            lng = -lng
+        return lng
+    
+    def __init__(self, lng, lat, lngdir=None, latdir=None):
         self._lng = lng
         self._lat = lat
+        self._lngdir = lngdir
+        self._latdir = latdir
         
     def getlng(self):
         return self._lng
@@ -13,12 +61,20 @@ class LatLng(object):
         return self._lat
     lat = property(fget=getlat)
 
+    def getlngdir(self):
+        return self._lngdir
+    lngdir = property(fget=getlngdir)
+
+    def getlatdir(self):
+        return self._latdir
+    latdir = property(fget=getlatdir)
+
     def valid(self):
         return self.lat >= -90 and self.lat <= 90 \
             and self.lng >= -180 and self.lng <= 180
 
     def __str__(self):
-        return 'Lng=%s, Lat=%s' % (self.lat, self.lng)
+        return str(self.__dict__)
 
 class Coordinates(object):
     
@@ -53,15 +109,7 @@ class Coordinates(object):
         return self._config.get('unit')
     unit = property(fget=get_unit)
 
-        
-        
-
 if __name__ == '__main__':
     c = Coordinates({'point': LatLng(1, 2)})
     print c.point
 
-#class Calculator(object):
-#    
-#    @staticmethod
-#    def calculateCoordinatePrecision(Coordinates coordinates):
-#        pass
