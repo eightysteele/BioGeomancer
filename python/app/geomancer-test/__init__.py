c6d296106c33fefe7abc24b97e09e1cb7f34164e
Aaron Steele <eightysteele@gmail.com>
2011-06-03 05:52:02 -0700

    Fixed import geomancer error, added Heading class, dynamic loading of constants.Headings now working.

--- python/app/geomancer-test/__init__.py
+++ python/app/geomancer-test/__init__.py
@@ -1,4 +1,20 @@
+import sys
+import os
+path = os.path.abspath(os.path.dirname(os.path.realpath(__file__))).replace('app/geomancer-test', 'app')
+sys.path.append(path)
+print sys.path
+
 import geomancer
+import logging
+import unittest
+
+class TestGeomancer(unittest.TestCase):
+    def test_headings(self):
+        headings = geomancer.constants.Headings.all()
+        for heading in headings:
+            logging.info(heading)
 
 if __name__ == '__main__':
-    geomancer.test_georef()
+    logging.basicConfig(level=logging.DEBUG)
+    unittest.main()
+
